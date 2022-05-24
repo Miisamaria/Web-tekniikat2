97831a45bfe9ce2258b9d8dfd0235d1e70cf8260
Miisamaria <m.sammalkangas15@gmail.com>
2022-05-25 00:13:52 +0300

    scss luonti ja testaus. linkityksien päivitys

--- js/script.js
+++ js/script.js
@@ -9,5 +9,14 @@ fetch('https://dog.ceo/api/breeds/image/random')
 document.getElementById("yllätysnappi").addEventListener("click", myFunction);
 
 function myFunction() {
-  alert ("Hei wauu! Sie löysit minut! Ihanaa päivää just sulle!" + imgFunction);
+  alert ("Hei wauu! Sie löysit minut! Ihanaa päivää just sulle!");
 }
+document.body.style.backgroundColor = "#eadbc6";
+
+
+
+
+/*Apua haettu
+*https://www.delftstack.com/howto/javascript/javascript-change-background-color/
+*w3schools
+ */
