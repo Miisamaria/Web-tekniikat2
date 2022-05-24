7039e27ee91e8e0fd7fcf210a5224c247877fdcd
Miisamaria <m.sammalkangas15@gmail.com>
2022-05-24 16:18:43 +0300

    hienosäätöä ja kokeilua

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-fetch('https://programming-quotes-api.herokuapp.com/quotes/random')
+
+fetch('https://dog.ceo/api/breeds/image/random')
 .then(response => response.json())
 .then(data => {
      document.getElementById('print-here').innerHTML = 
      JSON.stringify(data);
-     document.getElementById('print-here').innerHTML += 
-     JSON.stringify(image);
     }
-);
+
+    );
+ 
