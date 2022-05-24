a2cd2ba716cb834dad0bdbb508c54ecf48b4853e
Miisamaria <m.sammalkangas15@gmail.com>
2022-05-25 00:58:04 +0300

    Viimeistelyt...Sass lisäykset, selectorit, eventit

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@
 fetch('https://dog.ceo/api/breeds/image/random')
 .then(response => response.json())
 .then(data => {
-     document.getElementById('print-here').innerHTML = 
-     '<img src="' + data.message + '"/>';
+     document.getElementById('print-here').innerHTML = '<img src="' + data.message + '"/>';
 });
 
-document.getElementById("yllätysnappi").addEventListener("click", myFunction);
 
 function myFunction() {
   alert ("Hei wauu! Sie löysit minut! Ihanaa päivää just sulle!");
-}
+};
+
+document.getElementById("yllätysnappi").addEventListener("click", myFunction);
+
 document.body.style.backgroundColor = "#eadbc6";
 
 
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 Ekana päivänä käyty kurssin diat ja mallit läpi
 Tokana päivänä suunniteltu mitä haluaa lähteä kokeileen
 Kolmantena päivänä sit toteutus...ja toteamiset, ettei oo taitotaso vielä sillä tasolla mitä haluisin tälleen pienenä perfektionistina.
+Aikataulu veny, koska sain ratkaistua ongelmia ja toteutettua asioita miten halusin! Eli neljäs päivä oli paras päivä!
